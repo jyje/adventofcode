96bd014f754bdbf103b8ca7d12e27842b4b30665
jyje <jyjeon@outlook.com>
2024-12-26 11:03:17 +0900

    init: complete day 5 (need to be fixed for time complexity for s2)

--- python/2024/5/main.py
+++ python/2024/5/main.py
@@ -1,36 +1,27 @@
-import sys, argparse
-from tabnanny import check
-
-parser = argparse.ArgumentParser()
-parser.add_argument("solution", action="store", default=1, type=int, help="Solution number. 1,2,3,...")
-parser.add_argument("file_path", action="store", type=str, help="File path of input data")
-
-args = parser.parse_args()
+import argparse
 
 def get_data(file_path: str) -> list:
     with open(file_path, "r") as file:
         split_data = file.read().split("\n\n")
         rules = [ list(map(int, rule.split("|"))) for rule in split_data[0].splitlines() ]
-        pages = split_data[1].splitlines()
+        pages = [ list(map(int, page.split(","))) for page in split_data[1].splitlines() ]
         return rules, pages
 
 
-def check_pages(table: dict, page: list) -> int:
-    data = list(map(int, page.split(",")))
+def check_pages(table: dict, page: list) -> (bool, int):
+    l = len(page)
 
-    l = len(data)
-
-    for index, key in enumerate(data):
+    for index, key in enumerate(page):
         if index == l-1:
             break
-        for next_page in data[index+1:]:
+        for next_page in page[index+1:]:
             pattern = table.get(key)
             if pattern:
                 if next_page not in table[key]:
-                    return -1
+                    return (False, -1)
             else:
-                return -1
-    return data[(l-1)//2]
+                return (False, -1)
+    return (True, page[(l-1)//2])
 
 
 def solution_1(rules: list, pages: list) -> int:
@@ -49,10 +40,31 @@ def solution_1(rules: list, pages: list) -> int:
     answer = 0
     for page in pages:
         result = check_pages(rule_table, page)
-        if result > 0:
-            answer += result
+        if result[0]:
+            answer += result[1]
 
     return answer
+
+
+def sort_page(table: dict, page: list, visited: list) -> list:
+    # print("current_visited:",visited)
+    if len(visited) == len(page) and check_pages(table, visited)[0]:
+        return visited
+
+    for remain in set(page) - set(visited):
+        if len(visited) == 0 or visited[-1] not in table.keys() or remain in table[visited[-1]]:
+            result = sort_page(table, page, visited + [remain])
+            if result is not None:
+                return result
+
+
+def sorted_middle_page(table: dict, page: list) -> int:
+    # print("page:",page)
+    # print("table:",table)
+    l = len(page)
+    valid_page = sort_page(table, page, [])
+    # print("valid_page:",valid_page)
+    return valid_page[(l-1)//2]
 
 
 def solution_2(rules: list, pages: list) -> int:
@@ -60,15 +72,33 @@ def solution_2(rules: list, pages: list) -> int:
     https://adventofcode.com/2024/day/5#part2
     """
 
-    return -1
+    # print("rules:", rules)
+    # print("---")
+    # print("pages:", pages)
+
+    rule_table = {}
+    for rule in rules:
+        rule_table.setdefault(rule[0], set()).add(rule[1])
+
+    answer = 0
+    for page in pages:
+        result = check_pages(rule_table, page)
+        if not result[0]:
+            answer += sorted_middle_page(rule_table, page)
+
+    return answer
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("solution", action="store", default=1, type=int, help="Solution number. 1,2,3,...")
+    parser.add_argument("file_path", action="store", type=str, help="File path of input data")
 
+    args = parser.parse_args()
     if args.solution == 1:
         solution = solution_1
-    # elif args.solution == 2:
-    #     solution = solution_2
+    elif args.solution == 2:
+        solution = solution_2
     else:
         raise ValueError("Wrong Solution")
 
--- python/2024/5/test.py
+++ python/2024/5/test.py
@@ -1,22 +1,27 @@
-import pytest
 from pathlib import Path
 
 from .main import solution_1, solution_2, get_data
 
+def test_sample_1():
+    file_path = Path(__file__).parent/"sample.txt"
+    expected = 143
+    rules, pages = get_data(file_path)
+    assert solution_1(rules, pages) == expected
+
 def test_input_1():
     file_path = Path(__file__).parent/"input.txt"
-    expected = 2547
-    data = get_data(file_path)
-    assert solution_1(data) == expected
+    expected = 7074
+    rules, pages = get_data(file_path)
+    assert solution_1(rules, pages) == expected
 
-def test_example_2():
-    file_path = Path(__file__).parent/"sample_2.txt"
-    expected = 9
-    data = get_data(file_path)
-    assert solution_2(data) == expected
+def test_sample_2():
+    file_path = Path(__file__).parent/"sample.txt"
+    expected = 123
+    rules, pages = get_data(file_path)
+    assert solution_2(rules, pages) == expected
 
 def test_input_2():
     file_path = Path(__file__).parent/"input.txt"
-    expected = 1939
-    data = get_data(file_path)
-    assert solution_2(data) == expected
+    expected = 0
+    rules, pages = get_data(file_path)
+    assert solution_2(rules, pages) == expected
